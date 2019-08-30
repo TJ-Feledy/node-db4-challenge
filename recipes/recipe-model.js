90d5b956083d37a9aaea0162326b070033a16c03
teej387 <teej387@yahoo.com>
2019-08-30 18:21:53 -0500

    Created getShoppingList() helper method

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -14,9 +14,9 @@ function getShoppingList(recipe_id) {
   if (recipe_id) {
     return db('quantities as q')
       .join('recipes as r', 'r.id', 'q.recipe_id')
-      .join('ingredients as ing', 'ing.id', 'ingredient_id')
-      .select('r.recipe', 'ing.ingredient', 'q.quantity', 'q.measurement')
-      .where(recipe_id = 'q.recipe_id')
+      .join('ingredients as ing', 'ing.id', 'q.ingredient_id')
+      .select('ing.ingredient', 'q.quantity', 'q.measurement')
+      .where({recipe_id})
   }else {
     return null
   }
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 
-const db = require('./data/db-config.js');
+const Recipes = require('./recipes/recipe-model.js');
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/api/recipes', (req, res) => {
-  db('recipes as r')
-    .join('quantities as q', 'q.recipe_id', 'r.id')
-    .join('ingredients as ing', 'ing.id', 'q.ingredient_id')
-    // .join('instructions as ins', 'ins.recipe_id', 'r.id')
-    .select('r.recipe', 'ing.ingredient', 'q.quantity', 'q.measurement')
+server.get('/api/recipes/:id', (req, res) => {
+  const {id} = req.params
+
+  Recipes.getShoppingList(id)
     .then(recipes => {
+      console.log(recipes)
       res.json(recipes)
     })
     .catch(err => {
